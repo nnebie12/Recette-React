e991a5aa8137dfd23e4674b6b8d8fb11c7f3d275
CODIPHENIX <bwangoastrid@gmail.com>
2025-11-20 21:08:40 +0100

    correct import issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ import RemoveRecette from './components/CrudRecette/RemoveRecette';
 import NavBar from './components/Layout/NavBar';
 import Card from './components/Card/Card';
 import Layout from './components/Layout/wrapper';  
-
 import RecipeFilter from './components/filtreRecette';
-import NavBar from './components/Layout/NavBar';
 
 // ...existing code...
 function App() {
